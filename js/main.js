c8b8813780b636d172785014e8798b5eeadcfc0c
Jay Freestone <mail@jayfreestone.com>
2016-10-14 15:51:22 +0100

    Different style loader

--- js/main.js
+++ js/main.js
@@ -10,10 +10,11 @@ $('document').ready(function(){
 	$progressBarBg = $progressBar.find('.progress-bar__bg');
 	$progressEta = $('.progress-eta');
 	$progressEtaSeconds = $progressEta.find('.progress-eta__seconds');
+    $progressLines = $progressBar.find('.progress-bar__line');
 
 	// Colors
 	var green = $('body').css('background-color');
-	var darkGreen = $progressBar.css('background-color');
+	var darkGreen = '#1a7566';
 
 	// Intro
 	tl.set($intro, {opacity: 1});
@@ -24,32 +25,39 @@ $('document').ready(function(){
 	tl.to($introCopy, .5, {opacity: 1, y: '0%'});
 
 	// First make it green so it's invisible
-	tl.set($progressBar, {backgroundColor: green});
+	// tl.set($progressBar, {backgroundColor: green});
 	tl.set($progressBarIcons, {y: '20%', opacity: 0, scale: .8});
-	tl.set($progressBarCaptions, {color: green});
+	tl.set($progressLines, {opacity: 0});
+	tl.set($progressBarCaptions, {opacity: 0});
 
 	// Show the parent
 	tl.set($progressBar, {opacity: 1});
 
 	// Show the icons
-	tl.staggerTo($progressBarIcons, 1, {y: '0%', opacity: 1, scale: 1, ease:Back.easeInOut}, 0.3)
-	  .to($progressBar, 1, {backgroundColor: darkGreen});
+	tl.staggerTo($progressBarIcons, 1, {y: '0%', opacity: 1, scale: 1, ease:Back.easeInOut}, 0.3, 'showBar');
+	tl.staggerTo($progressLines, 1, {opacity: .5, ease:Back.easeInOut}, 0.3, 'showBar');
 
-	tl.to($progressBarCaptions, 1, {color: darkGreen}, 'appear');
+	tl.to($progressBarCaptions, .5, {opacity: 1}, 'appear');
 
 	// Prep the BG fill on the icon parent
-	// tl.set($progressBarSections, {backgroundColor: darkGreen});
+	tl.set($progressBarSections, {backgroundColor: darkGreen});
 	tl.set($progressBarIcons.find('#icon'), {fill: 'transparent'});
 
+	// For each section, run the fill sequence
+	$progressBarSections.each(function(i, section){
+		tl.to($(section).find('.progress-bar__icon-bg'), 6, {scaleX: 1, ease: Power2.easeIn});
+		tl.to($(section).find('.progress-bar__icon-label'), 1, {color: '#fff'});
+		tl.to($(section).next('.progress-bar__line'), 1, {backgroundColor: '#fff'});
+	});
+
+	// Coundown timer
 	var counter = { var: parseInt($progressEtaSeconds.text()) };
 
-	tl.to(counter, 40, {
+	tl.to(counter, 32, {
 		var: 0,
 		onUpdate: function() {
 			$progressEtaSeconds.text(Math.ceil(counter.var));
 		},
 		ease: Power4.easeOut,
-	}, 'countdown');
-
-	tl.to($progressBarBg, 40, {scaleX: 1, ease: Power4.easeOut}, 'countdown');
+	});
 });
